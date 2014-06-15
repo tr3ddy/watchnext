9a0df9bba8c9695ff845195af53b8df9520fd969
Fabian Wiedemann <wifab@hrz.tu-chemnitz.de>
2014-06-15 11:57:53 +0200

    simple watchnextLists api

--- app.js
+++ app.js
@@ -1,51 +1,54 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var passwordHash = require('password-hash');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    passwordHash = require('password-hash'),
+    mongoskin = require('mongoskin');
+
 var app = express();
+app.use(bodyParser());
 
-var mongoose = require('mongoose');
+var db = mongoskin.db('mongodb://localhost:27017/watchnext', {safe:true})
 
-mongoose.connect('mongodb://localhost:27017/watchnext');
 
-var Schema = mongoose.Schema;
-
-var User = new Schema({
-    id: {type: String, required: true},
-    saltedHash: {type: String, required: true}
-});
-
-var UserModel = mongoose.model('User', User);
-
-app.use(bodyParser.json());
-
-app.route('/api/users')
+app.route('/api/watchnextLists')
     .all(function (req, res, next) {
-        next();
+        req.watchnextLists = db.collection('watchnextLists')
+        next()
     })
-    .get(function(req, res, next){
-        return UserModel.find(function (err, users) {
-            if(!err){
-                return res.send(users)
-            }else{
-                return console.log(err);
-            }
+    .get(function (req, res, next) {
+        req.watchnextLists.find({}, {limit:10, sort: [['_id',-1]]}).toArray(function (e, results) {
+            if(e) return next(e)
+            res.send(results)
         })
     })
     .post(function (req, res, next) {
-        var hash = passwordHash.generate(req.body.password);
-        var user = new UserModel({
-            id: req.body.id,
-            saltedHash: hash
-        });
-        user.save(function (err){
-            if(!err){
-                return console.log("created");
-            }else {
-                return console.log(err);
-            }
-        });
-        return res.send(user);
-    });
+        req.watchnextLists.insert(req.body, {}, function (e, results) {
+            if (e) return next(e)
+            res.send(results)
+        })
+    })
 
+app.route('/api/watchnextLists/:id')
+    .all(function (req, res, next) {
+        req.watchnextLists = db.collection('watchnextLists')
+        next()
+    })
+    .get(function (req, res, next) {
+        req.watchnextLists.findById(req.params.id, function (e, result) {
+            if (e) return next(e)
+            res.send(result)
+        })
+    })
+    .put(function (req, res, next) {
+        req.watchnextLists.updateById(req.params.id, {$set: req.body}, {safe:true, multi:false}, function (e, result) {
+            if (e) return next(e)
+            res.send((result===1)?{msg:'success'}:{msg:'error'})
+        })
+    })
+    .delete(function (req, res, next) {
+        req.watchnextLists.removeById(req.params.id, function (e, result) {
+            if (e) return next(e)
+            res.send((result === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
+    })
 
 app.listen(3000);
